d045e18a8ac1cd9043f96ec194711281f6f55a32
Ubuntu <ubuntu@ip-172-31-84-112.ec2.internal>
2022-04-12 13:41:26 +0000

    Done with  basic operations

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,13 @@
 class TasksController < ApplicationController
+  #index methods show collections of task , bascially all tasks
   def index
     @object = "crazy funky hat"
-    @tasks = Task.order(:description).limit(5)
+    @tasks = Task.order(:description)
+    #.limit(5)
+  end
+  
+  #show method shows single task which is a member action
+  def show
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "tasks#index"
-  get "tasks/:id" => "tasks/show"
+  get "goofers/:id" => "tasks#show", as: 'task'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
